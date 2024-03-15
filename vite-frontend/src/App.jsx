de5a3e7c8c1ab4a5a077ad33849616e5fcd27f64
yugal1107 <yugal1107@gmail.com>
2024-03-15 21:56:57 +0530

    Added at app.JSX

--- vite-frontend/src/App.jsx
+++ vite-frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Map from './components/Map'
 import User from './components/User'
 import Admin from './components/Admin'
 import {BrowserRouter , Routes , Route} from 'react-router-dom'
+import Home from './components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
